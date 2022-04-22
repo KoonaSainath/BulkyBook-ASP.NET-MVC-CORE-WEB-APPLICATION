649b4d2170f172bb92a01a912c0750da2ad61c8b
KoonaSainath <sainathwnp@gmail.com>
2022-04-22 18:41:00 +0530

    Included the required data annotations for Category.cs model class

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,10 +1,19 @@
-﻿namespace BulkyBookWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace BulkyBookWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter category name")]
+        [StringLength(maximumLength: 20, MinimumLength = 3, ErrorMessage = "Please enter category name of length 3 to 20 characters")]
         public string Name { get; set; }
-        public int DisplayOrder { get; set; }
+
+        [Required(ErrorMessage = "Please enter display ordeer")]
+        [Range(minimum: 1, maximum: 100, ErrorMessage = "Please enter display order from 1 to 100 only")]
+        public int? DisplayOrder { get; set; }
+
         public DateTime MyProperty { get; set; } = DateTime.Now;
     }
 }
