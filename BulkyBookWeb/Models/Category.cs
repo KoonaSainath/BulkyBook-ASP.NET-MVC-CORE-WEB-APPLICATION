348ff7c35c975c7e5ca8e01c46a91d46a6160f65
KoonaSainath <sainathwnp@gmail.com>
2022-04-23 13:21:39 +0530

    Added Display name data annotation to properties Name and DisplayOrder in Category.cs model

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -8,10 +8,12 @@ namespace BulkyBookWeb.Models
 
         [Required(ErrorMessage = "Please enter category name")]
         [StringLength(maximumLength: 20, MinimumLength = 3, ErrorMessage = "Please enter category name of length 3 to 20 characters")]
+        [Display(Name = "Category name")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Please enter display ordeer")]
         [Range(minimum: 1, maximum: 100, ErrorMessage = "Please enter display order from 1 to 100 only")]
+        [Display(Name = "Display order")]
         public int? DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
