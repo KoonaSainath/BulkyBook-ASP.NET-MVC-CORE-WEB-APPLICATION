0bb9d17ace4a33074f57cf743b2d47e7ec0c21af
KoonaSainath <sainathwnp@gmail.com>
2022-04-24 11:52:59 +0530

    Implemented POST method for EditCategory on submit

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -29,7 +29,7 @@ namespace BulkyBookWeb.Controllers
         public IActionResult CreateCategory(Category category)
         {
             //Custom validation (server-side)
-            if(category.Name == category.DisplayOrder.ToString())
+            if (category.Name == category.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("Name", "Please ensure that category name is different from display order");
                 ModelState.AddModelError("DisplayOrder", "Please ensure that display order is different from category name");
@@ -46,7 +46,7 @@ namespace BulkyBookWeb.Controllers
         [HttpGet]
         public IActionResult EditCategory(int? id)
         {
-            if(id == null || id == 0)
+            if (id == null || id == 0)
             {
                 return NotFound();
             }
@@ -55,12 +55,29 @@ namespace BulkyBookWeb.Controllers
             //Category categorySingle = db.Categories.SingleOrDefault(c => c.Id == id);
             //Category categoryFirst = db.Categories.FirstOrDefault(c => c.Id == id);
 
-            if(category == null)
+            if (category == null)
             {
                 return NotFound();
             }
 
             return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult EditCategory(Category category)
+        {
+            if(category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Category name cannot be same as display order");
+                ModelState.AddModelError("DisplayOrder", "Display order cannot be same as category name");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Categories.Update(category);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
         }
     }
 }
