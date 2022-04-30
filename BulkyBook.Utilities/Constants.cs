e93d7c679ceab1f6dec099552e4d5da54e8fbae4
KoonaSainath <sainathwnp@gmail.com>
2022-04-30 16:50:19 +0530

    restructured the solution by creating multiple projects (Models, Utilities and DataAccessLayer)

---
+++ BulkyBook.Utilities/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Utilities
+{
+    public static class Constants
+    {
+        public static string TOASTR_SUCCESS = "success";
+        public static string TOASTR_FAILURE = "failure";
+    }
+}
--- BulkyBookWeb/Constants/ApplicationConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BulkyBookWeb.Constants
-{
-    public class ApplicationConstants
-    {
-        public static string TOASTR_SUCCESS = "success";
-        public static string TOASTR_FAILURE = "failure";
-    }
-}
