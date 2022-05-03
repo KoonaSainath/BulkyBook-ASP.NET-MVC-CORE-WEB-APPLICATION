ecb92a2a2de6af716871a4e82ffea9b6f3a1cd02
KoonaSainath <sainathwnp@gmail.com>
2022-05-03 16:17:37 +0530

    Started repository pattern implementation. Created and implemented IRepository generic interface and Repository generic class

---
+++ BulkyBook.DataAccessLayer/Repository/IRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccessLayer.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        void AddItem(T item);
+        T GetItemByExpression(Expression<Func<T, bool>> filter);
+        void RemoveItem(T item);
+        void RemoveItemsInRange(IEnumerable<T> items);
+    }
+}
