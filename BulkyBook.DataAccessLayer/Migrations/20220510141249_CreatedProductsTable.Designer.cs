04172aaf5787a68990065d7a683d708685e66c3e
KoonaSainath <sainathwnp@gmail.com>
2022-05-10 19:44:06 +0530

    Created Products table by adding DbSet of Products property in the ApplicationDbContext

--- BulkyBook.DataAccessLayer/Migrations/20220510141249_CreatedProductsTable.Designer.cs
+++ BulkyBook.DataAccessLayer/Migrations/20220510141249_CreatedProductsTable.Designer.cs
@@ -4,6 +4,7 @@ using BulkyBookWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BulkyBook.DataAccessLayer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220510141249_CreatedProductsTable")]
+    partial class CreatedProductsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
